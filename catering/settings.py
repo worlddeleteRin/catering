67fa14fdd2f55f816362b7e1c2a1e01fadd68624
Rin NoName <noname@MacBook-Pro-Rin.local>
2020-12-14 12:54:48 +0300

    new commit

--- catering/settings.py
+++ catering/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '%r@+q%jmsub*@+%an2(oqp_8a1-44r!qdj#=4!roo+&3&0(re('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
